55a2f1c1a2dedc9f03c693271cedf0f5bb449d01
kidusmworku <kidus.m.worku@gmail.com>
2021-11-30 16:55:36 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: XylophonePage(),
+    ),
+  );
+}
+
+class XylophonePage extends StatelessWidget {
+  const XylophonePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
